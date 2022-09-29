807a0dd74fef488032eff4b6ba3c66d648e08de0
El Linzer <elinzer5@gmail.com>
2022-09-29 08:00:41 -0500

    finally seeded the db

--- app/models/card.py
+++ app/models/card.py
@@ -7,14 +7,13 @@ class CardModel(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable=False)
-    colors = db.Column(db.String, nullable=False)
     type = db.Column(db.String, nullable=False)
     rarity = db.Column(db.String, nullable=False)
     set = db.Column(db.String, nullable=False)
-    text = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=True)
     flavor = db.Column(db.String, nullable=True)
     artist = db.Column(db.String, nullable=False)
-    img_url = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String(1000), nullable=True)
     deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=True)
 
     created_at = db.Column(db.DateTime, default=func.now())
--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -3,18 +3,29 @@ from app.models import db
 from mtgsdk import Card
 
 def seed_cards():
-    cards = Card.where(set='afr').where(page=1).where(pageSize=2).array()
+    cards = Card.where(set='afr').where(page=1).where(pageSize=70).array()
 
     for card in cards:
+
+        if 'flavor' not in cards:
+            flavor = ' '
+        else:
+            flavor = card['flavor']
+
+        if 'imageUrl' not in card:
+            img = 'missing img'
+        else:
+            img = card['imageUrl']
+
         new_card = CardModel(name=card["name"],
-                             colors=card.colors,
-                             type=card.type,
-                             rarity=card.rarity,
-                             set=card.set,
-                             text=card.text,
-                             flavor=card.flavor,
-                             artist=card.artist,
-                             img_url=card.imageUrl,
+                             type=card["type"],
+                             rarity=card["rarity"],
+                             set=card["set"],
+                             text=card["text"],
+                             flavor=flavor,
+                             artist=card["artist"],
+                             img_url=img,
+                             deck_id=None
                             )
         db.session.add(new_card)
     db.session.commit()
