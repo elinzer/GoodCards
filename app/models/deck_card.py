ac1f77052da9663804dc07411bf37adb9f767f83
El Linzer <elinzer5@gmail.com>
2022-09-30 14:16:07 -0500

    finally got join table up and running

--- app/models/deck_card.py
+++ app/models/deck_card.py
@@ -3,6 +3,6 @@ from .db import db
 deck_cards = db.Table(
     'deck_cards',
     db.Model.metadata,
-    db.Column('deck_id', db.Integer, db.ForeignKey('decks.id')),
-    db.Column('card_id', db.Integer, db.ForeignKey('cards.id'))
+    db.Column('deck_id', db.Integer, db.ForeignKey('decks.id'), primary_key=True),
+    db.Column('card_id', db.Integer, db.ForeignKey('cards.id'), primary_key=True)
 )
--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -1,9 +1,17 @@
-from app.models import CardModel
+from app.models import CardModel, Deck, deck
 from app.models import db
 from mtgsdk import Card
+from random import choice
 
 def seed_cards():
     cards = Card.where(set='afr').where(page=1).where(pageSize=70).array()
+
+    deck1 = Deck.query.get(1)
+    deck2 = Deck.query.get(2)
+    deck3 = Deck.query.get(3)
+    deck4 = Deck.query.get(4)
+
+    deck_list = [deck1, deck2, deck3, deck4]
 
     for card in cards:
 
@@ -26,6 +34,7 @@ def seed_cards():
                              artist=card["artist"],
                              img_url=img,
                             )
+        new_card.decks.append(choice(deck_list))
         db.session.add(new_card)
     db.session.commit()
 
