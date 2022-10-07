2ff4c58ef0dd10bdea4b42a4827892ed5c316960
El Linzer <elinzer5@gmail.com>
2022-10-07 11:22:09 -0500

    footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -3,7 +3,25 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            It's a footer!
+            <div className='footer-container'>
+                <span>
+                    <div className="devTitle">Contact the Developer</div>
+                    <div className="dev-links">
+                        <a href="https://github.com/elinzer" target="_blank">
+                            <i className="fab fa-github fa-2xl" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/elinzer/" target="_blank">
+                            <i className="fab fa-linkedin fa-2xl" />
+                        </a>
+                    </div>
+                </span>
+                <span>
+          <div className="devTitle">GoodCards Github</div>
+          <a href="https://github.com/elinzer/GoodCards" id="repo" target="_blank">
+            <i className="fab fa-github fa-2xl" />
+          </a>
+        </span>
+            </div>
         </div>
     )
 }
--- react-app/src/components/Splash/SplashPage.js
+++ react-app/src/components/Splash/SplashPage.js
@@ -2,27 +2,12 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import Decks from '../Decks/Decks';
 import './SplashPage.css';
-import splashHeader from '../../images/mtgSplash.png';
+import splashHeader from '../../images/splashWText.png';
 
 
 const SplashPage = () => {
 
     const sessionUser = useSelector(state => state.session.user)
-    // let sessionLinks;
-    // if (!sessionUser) {
-    //     sessionLinks = (
-    //         // <div className="session-links">
-    //         //     <div>
-    //         //     <NavLink to='/login' exact={true} activeClassName='active'>
-    //         //         Login
-    //         //     </NavLink>
-    //         //     </div>
-    //         //     <NavLink to='/sign-up' exact={true} activeClassName='active'>
-    //         //         Sign Up
-    //         //     </NavLink>
-    //         // </div>
-    //     )
-    // }
 
     return (
         <div className="splash-container">
