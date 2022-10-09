662e4f7b3865494b505ea094936726435320c271
El Linzer <elinzer5@gmail.com>
2022-10-09 15:36:00 -0500

    final css please

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -39,6 +39,7 @@ const LoginForm = () => {
   return (
     <div className='login-form-container'>
       <div className='form-and-link'>
+        <div className='log-in-head'>Log In</div>
         <form onSubmit={onLogin} className='login-form'>
           <div>
             {errors.map((error, ind) => (
